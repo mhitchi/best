b3be1e3bb8990f672584dfa617747a8c5a301c92
Manning Hitchings <mayhitchings@gmail.com>
2023-12-19 20:09:31 -0500

    vertical scroll horizontal movement working

--- my-app/src/components/Layout/index.js
+++ my-app/src/components/Layout/index.js
@@ -1,9 +1,11 @@
 import './index.scss';
 import Sidebar from '../Sidebar';
+import { useRef, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollToPlugin, ScrollTrigger } from 'gsap/ScrollTrigger';
 
+gsap.registerPlugin(ScrollTrigger);
 
 const Layout = () => {
     gsap.registerPlugin(ScrollTrigger);
@@ -14,17 +16,7 @@ const Layout = () => {
     const sections = gsap.utils.toArray('.containerScroll .parent li');
     const texts = gsap.utils.toArray('.anim');
     const mask = document.querySelector('.mask');
-
-    let scrollTween = gsap.to(sections, {
-        xPercent: -100 * (sections.length - 1),
-        ease: 'none',
-        scrollTrigger: {
-            trigger: '.parent',
-            pin: true,
-            scrub: 1,
-            end: '+=3000'
-        }
-    })
+  
 
     gsap.to(mask, {
         width: "100%",
@@ -34,6 +26,59 @@ const Layout = () => {
           scrub: 1
         }
       });
+
+      //start test
+
+      const firstRef = useRef();
+      const secondRef = useRef();
+      const wrapperRef = useRef();
+      
+        useEffect(() => {
+        const ctx = gsap.context((self) => {  
+        const sections = self.selector("li");
+       
+    
+        const horizontalTween = gsap.to(sections, {
+          xPercent: -100 * (sections.length - 1),
+          ease: "none",
+          scrollTrigger: {
+            trigger: wrapperRef.current,
+            pin: true,
+            markers: true,
+            scrub: 1,
+            end: () => "+=" + document.querySelector(".page").offsetWidth
+          }
+        })
+    
+          gsap.to(firstRef.current, {
+          rotate: 90,
+          scale:3,
+            scrollTrigger: {
+              trigger:firstRef.current,
+              containerAnimation: horizontalTween,
+              markers:true,
+              start: "center center"
+            }
+        })
+          
+           gsap.to(secondRef.current, {
+          rotate: -90,
+             scale:3,
+            scrollTrigger: {
+              trigger:secondRef.current,
+               containerAnimation: horizontalTween,
+              markers:true,
+              start: "center center"
+            }
+        })
+          
+          }, wrapperRef);
+          return () => ctx.revert();
+          
+      }, []);
+
+
+      //end test
 
     // end scrolltrigger mask svg
 
@@ -66,12 +111,21 @@ const Layout = () => {
                 <path d="M9.89998 6C9.43671 8.28224 7.41896 10 5 10C2.23858 10 0 7.76142 0 5C0 2.23858 2.23858 0 5 0C7.41896 0 9.43671 1.71776 9.89998 4H445.1C445.563 1.71776 447.581 0 450 0C452.419 0 454.437 1.71776 454.9 4H890.1C890.563 1.71776 892.581 0 895 0C897.761 0 900 2.23858 900 5C900 7.76142 897.761 10 895 10C892.581 10 890.563 8.28224 890.1 6H454.9C454.437 8.28224 452.419 10 450 10C447.581 10 445.563 8.28224 445.1 6H9.89998Z" fill="#D9D9D9"/>
                 </mask>
                 <g mask="url(#mask0_0_1)">
-                <rect class="mask" y="-49" height="99" fill="black"/>
+                <rect className="mask" y="-49" height="99" fill="black"/>
                 </g>
             </svg>
-        <div className="page layout containerScroll" onWheel={handleScroll}>
+        <div className="page layout containerScroll" onWheel={handleScroll} ref={wrapperRef}>
             <span className="tags top-tags"></span>
-            
+
+
+{/* start */}
+{/* <div className="wrapper" ref={wrapperRef}>
+      <section className="header"> <h1>HEADER</h1></section>
+        <section className="first"><h2 ref={firstRef}>FIRST</h2></section>
+        <section className="second"><h2 ref={secondRef}>SECOND</h2></section>
+      </div> */}
+
+{/* end */}
             <Outlet />
             
         </div>
