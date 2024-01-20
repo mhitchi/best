f3b6f2448d3f55e7023fb26ad29ee7b72955a386
Manning Hitchings <mayhitchings@gmail.com>
2024-01-20 11:39:54 -0500

    removed mask up top

--- my-app/src/components/Layout/index.js
+++ my-app/src/components/Layout/index.js
@@ -25,17 +25,18 @@ const Layout = () => {
     // const container = document.querySelector('.containerScroll');
     // const sections = gsap.utils.toArray('.containerScroll .parent li');
     // const texts = gsap.utils.toArray('.anim');
-    const mask = document.querySelector('.mask');
+    // const mask = document.querySelector('.mask');
 
-    gsap.to(mask, {
-        width: "100%",
-        scrollTrigger: {
-          trigger: ".page",
-          start: "top left",
-          scrub: 1,
-          pinReparent: true
-        }
-      });
+    //tween to move mask
+    // gsap.to(mask, {
+    //     width: "100%",
+    //     scrollTrigger: {
+    //       trigger: ".page",
+    //       start: "top left",
+    //       scrub: 1,
+    //       pinReparent: true
+    //     }
+    //   });
       
       //start test
 
@@ -109,13 +110,13 @@ const Layout = () => {
     // end scrolltrigger mask svg
 
 
-    const maskStyle= {
-        maskType:"alpha", 
-        x:"0", 
-        y:"0", 
-        width:"900", 
-        height:"10"
-    }
+    // const maskStyle= {
+    //     maskType:"alpha", 
+    //     x:"0", 
+    //     y:"0", 
+    //     width:"900", 
+    //     height:"10"
+    // }
     const handleScroll = (event) => {
         const container = event.target;
         const scrollAmount = event.deltaY;
@@ -131,16 +132,7 @@ const Layout = () => {
     return (
         <>
         <div onClick={handleClick}><Sidebar/></div>
-        {/* scroll masked svg animation */}
-        <svg className="svg" viewBox="0 0 900 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9.89998 6C9.43671 8.28224 7.41896 10 5 10C2.23858 10 0 7.76142 0 5C0 2.23858 2.23858 0 5 0C7.41896 0 9.43671 1.71776 9.89998 4H445.1C445.563 1.71776 447.581 0 450 0C452.419 0 454.437 1.71776 454.9 4H890.1C890.563 1.71776 892.581 0 895 0C897.761 0 900 2.23858 900 5C900 7.76142 897.761 10 895 10C892.581 10 890.563 8.28224 890.1 6H454.9C454.437 8.28224 452.419 10 450 10C447.581 10 445.563 8.28224 445.1 6H9.89998Z" fill="#D9D9D9"/>
-                <mask id="mask0_0_1" style={maskStyle} maskUnits="userSpaceOnUse">
-                <path d="M9.89998 6C9.43671 8.28224 7.41896 10 5 10C2.23858 10 0 7.76142 0 5C0 2.23858 2.23858 0 5 0C7.41896 0 9.43671 1.71776 9.89998 4H445.1C445.563 1.71776 447.581 0 450 0C452.419 0 454.437 1.71776 454.9 4H890.1C890.563 1.71776 892.581 0 895 0C897.761 0 900 2.23858 900 5C900 7.76142 897.761 10 895 10C892.581 10 890.563 8.28224 890.1 6H454.9C454.437 8.28224 452.419 10 450 10C447.581 10 445.563 8.28224 445.1 6H9.89998Z" fill="#D9D9D9"/>
-                </mask>
-                <g mask="url(#mask0_0_1)">
-                <rect className="mask" y="-49" height="99" fill="black"/>
-                </g>
-            </svg>
+        
         <div className="page layout containerScroll" onWheel={handleScroll} ref={wrapperRef}>
             <span className="tags top-tags"></span>
 
